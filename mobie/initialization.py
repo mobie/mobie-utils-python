b33bad8f691ff2af6b6d5a43810e91903e7b1a11
Constantin Pape <c.pape@gmx.net>
2020-06-05 16:44:52 +0200

    Add dataset initialization WIP

---
+++ mobie/initialization.py
@@ -0,0 +1,140 @@
+import json
+import multiprocessing
+import os
+import warnings
+
+from .layer_settings import (default_image_layer_settings,
+                             validate_layer_settings)
+from .import_data import import_raw_volume
+
+
+def initialize_dataset(input_path, input_key,
+                       root, dataset_name, raw_name,
+                       resolution, chunks, scale_factors,
+                       is_default=False, tmp_folder=None, target='local',
+                       max_jobs=multiprocessing.cpu_count(), time_limit=None):
+    """ Initialize a MoBIE dataset by copying raw data and creating the dataset folder.
+
+    Arguments:
+        input_path [str] -
+    """
+    dataset_folder = make_dataset_folders(root, dataset_name)
+
+    if tmp_folder is None:
+        tmp_folder = f'tmp_{dataset_name}'
+    xml_path = os.path.join(dataset_folder, 'images', 'local', f'{raw_name}.xml')
+    import_raw_volume(input_path, input_key, xml_path,
+                      resolution, scale_factors, chunks,
+                      tmp_folder=tmp_folder, target=target, max_jobs=max_jobs)
+
+    initialize_image_dict(dataset_folder, xml_path)
+
+    initialize_bookmarks(dataset_folder, raw_name)
+
+    add_dataset(root, dataset_name, is_default)
+
+
+def make_dataset_folders(root, dataset_name):
+    """ Make the folder structure for a new dataset.
+
+    Arguments:
+        root [str] - the root data directory
+        dataset_name [str] - name of the dataset
+    """
+    dataset_folder = os.path.join(root, dataset_name)
+    os.makedirs(os.path.join(dataset_folder, 'tables'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'images', 'local'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'images', 'remote'), exist_ok=True)
+    os.makedirs(os.path.join(dataset_folder, 'misc'), exist_ok=True)
+    return dataset_folder
+
+
+def add_dataset(root, dataset_name, is_default):
+    path = os.path.join(root, 'datasets.json')
+    try:
+        with open(path) as f:
+            datasets = json.load(f)
+    except (FileNotFoundError, json.JsonDecodeError):
+        datasets = {}
+        datasets['datasets'] = []
+
+    if dataset_name in datasets['datasets']:
+        warnings.warn(f"Dataset {dataset_name} is already present!")
+    else:
+        datasets['datasets'].append(dataset_name)
+
+    if is_default:
+        datasets['defaultDataset'] = dataset_name
+
+    with open(path, 'w') as f:
+        json.dump(datasets, f, sort_keys=True, indent=2)
+
+
+def initialize_image_dict(folder, xml_path, layer_settings=None):
+    """ Initialize the image dict for a new dataset.
+
+    Arguments:
+        folder [str] - path to the dataset folder.
+        xml_path [str] - path to the xml for the raw data of this dataset.
+        layer_settings [dict] - settings for the raw layer. (default: None)
+    """
+
+    assert os.path.exists(xml_path), xml_path
+
+    image_folder = os.path.join(folder, 'images')
+    image_dict_path = os.path.join(image_folder, 'images.json')
+
+    raw_name = os.path.splitext(os.path.split(xml_path)[1])[0]
+    rel_path = os.path.relpath(xml_path, image_folder)
+
+    # TODO
+    if layer_settings is None:
+        layer_settings = default_image_layer_settings()
+    else:
+        validate_layer_settings(layer_settings)
+
+    layer_settings.update({
+        "storage": {
+            "local": rel_path,
+            "remote": rel_path.replace("local", "remote")
+        }
+    })
+
+    image_dict = {
+        raw_name: layer_settings
+    }
+
+    with open(image_dict_path, 'w') as f:
+        json.dump(image_dict, f, indent=2, sort_keys=True)
+
+
+def initialize_bookmarks(folder, raw_name, layer_settings=None):
+    """ Initialize the boomkmarks for a new dataset.
+
+    Arguments:
+        folder [str] - path to the dataset folder.
+        raw_name [str] - name of the raw data for this dataset.
+        layer_settings [dict] - settings for the layer. (default: None)
+    """
+
+    bookmark_folder = os.path.join(folder, 'misc', 'bookmarks')
+    os.makedirs(bookmark_folder, exist_ok=True)
+    bookmark_path = os.path.join(bookmark_folder, 'default.json')
+
+    if layer_settings is None:
+        layer_settings = {
+            'contrastLimits': default_image_layer_settings()['contrastLimits']
+        }
+    else:
+        validate_layer_settings(layer_settings)
+
+    bkmrk = {
+        'default': {
+            'layers': {
+                raw_name: layer_settings
+            }
+        }
+    }
+
+    with open(bookmark_path, 'w') as f:
+        json.dump(bkmrk, f, indent=2, sort_keys=True)
---
+++ mobie/xml_utils.py
@@ -0,0 +1,94 @@
+import xml.etree.ElementTree as ET
+from pybdv.metadata import get_data_path, indent_xml
+
+
+def copy_xml_with_abspath(xml_in, xml_out):
+    path = get_data_path(xml_in, return_absolute_path=True)
+    copy_xml_with_newpath(xml_in, xml_out, path,
+                          path_type='absolute')
+
+
+def copy_xml_with_newpath(xml_in, xml_out, data_path,
+                          path_type='relative', data_format='bdv.hdf5'):
+    assert path_type in ('absolute', 'relative')
+    # get the path node inn the xml tree
+    root = ET.parse(xml_in).getroot()
+    seqdesc = root.find('SequenceDescription')
+    imgload = seqdesc.find('ImageLoader')
+    imgload.set('format', data_format)
+    et = imgload.find('hdf5')
+    if et is None:
+        et = imgload.find('n5')
+    if et is None:
+        raise RuntimeError("Could not find data node")
+    et.tag = data_format.split('.')[-1]
+    et.text = data_path
+    et.set('type', path_type)
+
+    indent_xml(root)
+    tree = ET.ElementTree(root)
+    tree.write(xml_out)
+
+
+# should be generalized and moved to pybdv at some point
+def copy_xml_as_bdv_n5_s3(in_xml, out_xml,
+                          service_endpoint, bucket_name, path_in_bucket,
+                          authentication='Anonymous', region='us-west-2'):
+    """ Copy a bdv xml file and replace the image data loader with the bdv.n5.s3 format.
+
+    Arguments:
+        in_xml [str] - path to the input xml
+        out_xml [str] - path to the output xml
+        service_endpoint [str] - url of the s3 service end-point.
+            For EMBL: 'https://s3.embl.de'.
+        bucket_name [str] - name of the bucket
+        path_in_bucket [str] - file paths inside of the bucket
+        authentication [str] - the authentication mode, can be 'Anonymous' or 'Protected'.
+            Default: 'Anonymous'
+        region [str] - the region. Only relevant if aws.s3 is used.
+            Default: 'us-west-2'
+    """
+
+    auth_modes = ('Anonymous', 'Protected')
+    if authentication not in auth_modes:
+        raise ValueError(f"Invalid authentication mode {authentication}, expected one of {auth_modes}")
+
+    # check if we have an xml already
+    tree = ET.parse(in_xml)
+    root = tree.getroot()
+
+    # load the sequence description
+    seqdesc = root.find('SequenceDescription')
+
+    # update the image loader
+    # remove the old image loader
+    imgload = seqdesc.find('ImageLoader')
+    seqdesc.remove(imgload)
+
+    # write the new image loader
+    imgload = ET.SubElement(seqdesc, 'ImageLoader')
+    bdv_type = 'bdv.n5.s3'
+    imgload.set('format', bdv_type)
+    el = ET.SubElement(imgload, 'Key')
+    el.text = path_in_bucket
+
+    el = ET.SubElement(imgload, 'SigningRegion')
+    el.text = region
+    el = ET.SubElement(imgload, 'ServiceEndpoint')
+    el.text = service_endpoint
+    el = ET.SubElement(imgload, 'BucketName')
+    el.text = bucket_name
+    el = ET.SubElement(imgload, 'Authentication')
+    el.text = authentication
+
+    indent_xml(root)
+    tree = ET.ElementTree(root)
+    tree.write(out_xml)
+
+
+def read_path_in_bucket(xml):
+    root = ET.parse(xml).getroot()
+    seqdesc = root.find('SequenceDescription')
+    imgload = seqdesc.find('ImageLoader')
+    el = imgload.find('Key')
+    return el.text
