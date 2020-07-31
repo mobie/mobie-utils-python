1bd023f450bb294c6cc71f58f1e723014102ee0c
Constantin Pape <c.pape@gmx.net>
2020-07-31 23:20:29 +0200

    Add node labels functionality

--- mobie/import_data/util.py
+++ mobie/import_data/util.py
@@ -2,10 +2,47 @@ import json
 import os
 
 import luigi
-from cluster_tools.downscaling import DownscalingWorkflow
 from cluster_tools.statistics import DataStatisticsWorkflow
+from cluster_tools.downscaling import DownscalingWorkflow
+from cluster_tools.node_labels import NodeLabelWorkflow
 from elf.io import open_file
 from ..config import write_global_config
+
+
+def compute_node_labels(seg_path, seg_key,
+                        input_path, input_key,
+                        tmp_folder, target, max_jobs,
+                        ignore_label=None, max_overlap=True):
+    task = NodeLabelWorkflow
+    config_folder = os.path.join(tmp_folder, 'configs')
+
+    out_path = os.path.join(tmp_folder, 'data.n5')
+    out_key = 'node_labels_%s' % prefix
+
+    t = task(tmp_folder=tmp_folder, config_dir=config_folder,
+             max_jobs=max_jobs, target=target,
+             ws_path=seg_path, ws_key=seg_key,
+             input_path=input_path, input_key=input_key,
+             output_path=out_path, output_key=out_key,
+             prefix=prefix, max_overlap=max_overlap,
+             ignore_label=ignore_label)
+    ret = luigi.build([t], local_scheduler=True)
+    if not ret:
+        raise RuntimeError("Node labels for %s" % prefix)
+
+    f = open_file(out_path, 'r')
+    ds_out = f[out_key]
+
+    if max_overlap:
+        data = ds_out[:]
+    else:
+        n_chunks = ds_out.number_of_chunks
+        data = [ndist.deserializeOverlapChunk(out_path, out_key, (chunk_id,))[0]
+                for chunk_id in range(n_chunks)]
+        data = {label_id: overlaps
+                for chunk_data in data
+                for label_id, overlaps in chunk_data.items()}
+    return data
 
 
 def downscale(in_path, in_key, out_path,
