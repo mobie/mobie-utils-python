afe799000e30185d6dd48a6c6763bac2bdf8a309
Constantin Pape <c.pape@gmx.net>
2021-03-03 22:18:45 +0100

    Fix typo in example notebook

--- mobie/metadata/__init__.py
+++ mobie/metadata/__init__.py
@@ -1,5 +1,5 @@
 from .bookmarks import add_bookmark
 from .datasets import add_dataset, copy_dataset_folder, get_datasets,  have_dataset
-from .image_dict import add_to_image_dict
+from .image_dict import add_to_image_dict, load_image_dict
 from .transformation import update_transformation_parameter
 from .remote import add_remote_project_metadata
