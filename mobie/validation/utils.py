53f1e1bdae4fee5fa013ca4fcf06bf772a0d7784
Constantin Pape <c.pape@gmx.net>
2021-03-21 21:02:15 +0100

    Update validation util

--- mobie/validation/utils.py
+++ mobie/validation/utils.py
@@ -6,16 +6,18 @@ import jsonschema
 import requests
 
 
-# TODO jsonschema should be able to do this!
+SCHEMA_URLS = {
+    "bookmark": "https://raw.githubusercontent.com/mobie/mobie.github.io/master/schema/bookmark.schema.json",
+    "dataset": "https://raw.githubusercontent.com/mobie/mobie.github.io/master/schema/dataset.schema.json",
+    "project": "https://raw.githubusercontent.com/mobie/mobie.github.io/master/schema/project.schema.json",
+    "source": "https://raw.githubusercontent.com/mobie/mobie.github.io/master/schema/source.schema.json",
+    "view": "https://raw.githubusercontent.com/mobie/mobie.github.io/master/schema/view.schema.json"
+}
+
+
 def _download_schema():
     folder = os.path.expanduser('~/.mobie')
     os.makedirs(folder, exist_ok=True)
-
-    source_schema_address = ''
-    source_schema_file = os.path.join(folder, 'source.schema.json')
-
-    view_schema_address = ''
-    view_schema_file = os.path.join(folder, 'view.schema.json')
 
     def _download(address, out_file):
         if os.path.exists(out_file):
@@ -28,23 +30,27 @@ def _download_schema():
         except Exception:
             return False
 
-    if not _download(source_schema_address, source_schema_file):
-        return False
-    if not _download(view_schema_address, view_schema_file):
-        return False
+    for name, url in SCHEMA_URLS.items():
+        out_file = os.path.join(folder, f"{name}.schema.json")
+        if not _download(url, out_file):
+            return False
     return True
 
 
 def validate_with_schema(metadata, schema):
     assert isinstance(schema, (str, dict))
     if isinstance(schema, str):
-        assert schema in ("view", "source", "dataset", "project")
+        assert schema in SCHEMA_URLS
         if not _download_schema():
-            warnings.warn("TODO")
+            warnings.warn(f"Could not download the schema from {SCHEMA_URLS[schema]}. Check your internet connection.")
             return
         schema = os.path.expanduser(f'~/.mobie/{schema}.schema.json')
         with open(schema, 'r') as f:
             schema = json.load(f)
+
+        # I hoped this would work, but seems like this doesn't work
+        # schema = SCHEMA_URLS[schema]
+
     jsonschema.validate(instance=metadata, schema=schema)
 
 
