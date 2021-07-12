c17be154f7d197d02fdcb11afb5e53a24c5cd3dd
Constantin Pape <constantin.pape@embl.de>
2021-07-12 15:44:20 +0200

    Add blending mode to segmentation display keyword arguments

--- mobie/metadata/view_metadata.py
+++ mobie/metadata/view_metadata.py
@@ -34,7 +34,7 @@ def get_segmentation_display(name, sources, **kwargs):
         "name": name,
         "sources": sources
     }
-    additional_seg_kwargs = ["colorByColumn", "resolution3dView",
+    additional_seg_kwargs = ["blendingMode", "colorByColumn", "resolution3dView",
                              "selectedSegmentIds", "showSelectedSegmentsIn3d",
                              "tables", "valueLimits"]
     for kwarg_name in additional_seg_kwargs:
