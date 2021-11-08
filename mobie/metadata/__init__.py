54613723b123fdf1c2852a51a850bb399be4f44d
Constantin Pape <c.pape@gmx.net>
2021-11-08 10:49:25 +0100

    Add import in metadata

--- mobie/metadata/__init__.py
+++ mobie/metadata/__init__.py
@@ -3,7 +3,8 @@ from .dataset_metadata import (add_view_to_dataset, copy_dataset_folder,
                                create_dataset_structure, create_dataset_metadata,
                                read_dataset_metadata, set_is2d, write_dataset_metadata)
 from .project_metadata import (add_dataset, create_project_metadata,
-                               dataset_exists, get_file_formats, has_file_format, read_project_metadata,
+                               dataset_exists, get_datasets, get_file_formats,
+                               has_file_format, read_project_metadata,
                                project_exists, write_project_metadata)
 from .remote_metadata import (add_remote_dataset_metadata, add_remote_project_metadata, add_remote_source_metadata,
                               upload_source)
