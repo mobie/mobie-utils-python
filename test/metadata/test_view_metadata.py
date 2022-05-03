eea0c97e0310e4d2dc9b6dcdf480e437658e8a30
Constantin Pape <c.pape@gmx.net>
2022-05-03 08:54:18 +0200

    Cosmetics

--- test/metadata/test_view_metadata.py
+++ test/metadata/test_view_metadata.py
@@ -20,171 +20,171 @@ class TestViewMetadata(unittest.TestCase):
         from mobie.metadata import get_default_view
 
         # test the default view
-        view = get_default_view('image', 'my-image')
-        validate_with_schema(view, 'view')
+        view = get_default_view("image", "my-image")
+        validate_with_schema(view, "view")
 
         # test custom image settings
         custom_kwargs = [
-            {'contrastLimits': [0., 255.], 'color': 'white'},
-            {'contrastLimits': [0., 2000.], 'color': 'red'},
-            {'contrastLimits': [-10., 20000000.]},
-            {'showImagesIn3d': True},
-            {'showImagesIn3d': True, 'resolution3dView': [10., 10., 12.]},
-            {'blendingMode': 'sumOccluding'},
+            {"contrastLimits": [0., 255.], "color": "white"},
+            {"contrastLimits": [0., 2000.], "color": "red"},
+            {"contrastLimits": [-10., 20000000.]},
+            {"showImagesIn3d": True},
+            {"showImagesIn3d": True, "resolution3dView": [10., 10., 12.]},
+            {"blendingMode": "sumOccluding"},
         ]
         for kwargs in custom_kwargs:
-            view = get_default_view('image', 'my-image', **kwargs)
-            validate_with_schema(view, 'view')
+            view = get_default_view("image", "my-image", **kwargs)
+            validate_with_schema(view, "view")
 
         # test missing fields
-        view = get_default_view('image', 'my-image')
-        view['sourceDisplays'][0]['imageDisplay'].pop('color')
+        view = get_default_view("image", "my-image")
+        view["sourceDisplays"][0]["imageDisplay"].pop("color")
         with self.assertRaises(ValidationError):
-            validate_with_schema(view, 'view')
+            validate_with_schema(view, "view")
 
         # test invalid fields
-        view = get_default_view('image', 'my-image')
-        view['foo'] = 'bar'
+        view = get_default_view("image", "my-image")
+        view["foo"] = "bar"
         with self.assertRaises(ValidationError):
-            validate_with_schema(view, 'view')
+            validate_with_schema(view, "view")
 
-        view = get_default_view('image', 'my-image')
-        view['sourceDisplays'][0]['imageDisplay']["foo"] = "bar"
+        view = get_default_view("image", "my-image")
+        view["sourceDisplays"][0]["imageDisplay"]["foo"] = "bar"
         with self.assertRaises(ValidationError):
-            validate_with_schema(view, 'view')
+            validate_with_schema(view, "view")
 
         # test invalid values
         invalid_kwargs = [
-            {'uiSelectionGroup': "abc a"},
-            {'uiSelectionGroup': "abc/a"},
-            {'uiSelectionGroup': "abc;"},
-            {'color': "foobar"},
-            {'color': "r=1,g=2,b=3,a=4,z=5"},
-            {'contrastLimits': [1., 2., 3.]},
-            {'showImagesIn3d': "foobar"},
-            {'resolution3dView': [1., 2., 3., 4.]},
-            {'blendingMode': 'summe'}
+            {"uiSelectionGroup": "abc a"},
+            {"uiSelectionGroup": "abc/a"},
+            {"uiSelectionGroup": "abc;"},
+            {"color": "foobar"},
+            {"color": "r=1,g=2,b=3,a=4,z=5"},
+            {"contrastLimits": [1., 2., 3.]},
+            {"showImagesIn3d": "foobar"},
+            {"resolution3dView": [1., 2., 3., 4.]},
+            {"blendingMode": "summe"}
         ]
         for kwargs in invalid_kwargs:
-            if 'uiSelectionGroup' in kwargs:
-                menu_name = kwargs.pop('uiSelectionGroup')
-            view = get_default_view('image', 'my-image', menu_name=menu_name, **kwargs)
+            if "uiSelectionGroup" in kwargs:
+                menu_name = kwargs.pop("uiSelectionGroup")
+            view = get_default_view("image", "my-image", menu_name=menu_name, **kwargs)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
     def test_segmentation_view(self):
         from mobie.metadata import get_default_view
 
         # test the default view
-        view = get_default_view('segmentation', 'my-seg')
-        validate_with_schema(view, 'view')
+        view = get_default_view("segmentation", "my-seg")
+        validate_with_schema(view, "view")
 
         # test custom segmentation settings
         custom_kwargs = [
-            {'opacity': 0.5, 'lut': 'glasbey'},
-            {'opacity': 0.9, 'lut': 'viridis',
-             'colorByColumn': 'colname', 'showSelectedSegmentsIn3d': True, "tables": ["a.csv", "b.tsv"]},
-            {'selectedSegmentIds': ['my-seg;0;1', 'my-seg;0;2', 'my-seg;1;10']}
+            {"opacity": 0.5, "lut": "glasbey"},
+            {"opacity": 0.9, "lut": "viridis",
+             "colorByColumn": "colname", "showSelectedSegmentsIn3d": True, "tables": ["a.csv", "b.tsv"]},
+            {"selectedSegmentIds": ["my-seg;0;1", "my-seg;0;2", "my-seg;1;10"]}
         ]
         for kwargs in custom_kwargs:
-            view = get_default_view('segmentation', 'my-seg', **kwargs)
-            validate_with_schema(view, 'view')
+            view = get_default_view("segmentation", "my-seg", **kwargs)
+            validate_with_schema(view, "view")
 
         # test missing fields
-        view = get_default_view('segmentation', 'my-seg')
-        view['sourceDisplays'][0]['segmentationDisplay'].pop('opacity')
+        view = get_default_view("segmentation", "my-seg")
+        view["sourceDisplays"][0]["segmentationDisplay"].pop("opacity")
         with self.assertRaises(ValidationError):
-            validate_with_schema(view, 'view')
+            validate_with_schema(view, "view")
 
         # test invalid fields
-        view = get_default_view('segmentation', 'my-seg')
-        view['sourceDisplays'][0]['segmentationDisplay']["foo"] = "bar"
+        view = get_default_view("segmentation", "my-seg")
+        view["sourceDisplays"][0]["segmentationDisplay"]["foo"] = "bar"
         with self.assertRaises(ValidationError):
-            validate_with_schema(view, 'view')
+            validate_with_schema(view, "view")
 
         # test invalid values
         invalid_kwargs = [
-            {'opacity': 10},
-            {'lut': "red"},
-            {'lut': "foobar"},
-            {'selectedSegmentIds': ['my-seg,0,2']},
-            {'selectedSegmentIds': ['my-seg/abc;0;2']},
-            {'selectedSegmentIds': ['my-segc;abba;2']}
+            {"opacity": 10},
+            {"lut": "red"},
+            {"lut": "foobar"},
+            {"selectedSegmentIds": ["my-seg,0,2"]},
+            {"selectedSegmentIds": ["my-seg/abc;0;2"]},
+            {"selectedSegmentIds": ["my-segc;abba;2"]}
         ]
         for kwargs in invalid_kwargs:
-            view = get_default_view('segmentation', 'my-seg', **kwargs)
+            view = get_default_view("segmentation", "my-seg", **kwargs)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
         # test view with source transformations
         trafos = [
-            {'parameters': np.random.rand(12).tolist()},
-            {'parameters': np.random.rand(12).tolist(), 'timepoints': [0]},
-            {'parameters': np.random.rand(12).tolist(), 'timepoints': [1, 2, 3]}
+            {"parameters": np.random.rand(12).tolist()},
+            {"parameters": np.random.rand(12).tolist(), "timepoints": [0]},
+            {"parameters": np.random.rand(12).tolist(), "timepoints": [1, 2, 3]}
         ]
         for trafo in trafos:
-            view = get_default_view('image', 'my-image', source_transform=trafo)
-            validate_with_schema(view, 'view')
+            view = get_default_view("image", "my-image", source_transform=trafo)
+            validate_with_schema(view, "view")
 
         # test invalid values
         invalid_trafos = [
-            {'parameters': np.random.rand(12).tolist(), 'timepoints': [-1]},
-            {'parameters': np.random.rand(12).tolist(), 'timepoints': [-1, 2, 3]}
+            {"parameters": np.random.rand(12).tolist(), "timepoints": [-1]},
+            {"parameters": np.random.rand(12).tolist(), "timepoints": [-1, 2, 3]}
         ]
         for trafo in invalid_trafos:
-            view = get_default_view('image', 'my-image', source_transform=trafo)
+            view = get_default_view("image", "my-image", source_transform=trafo)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
     def test_viewer_transform(self):
         from mobie.metadata import get_default_view
 
         trafos = [
             {"timepoint": 0},
-            {'affine': np.random.rand(12).tolist()},
-            {'affine': np.random.rand(12).tolist(), "timepoint": 0},
-            {'normalizedAffine': np.random.rand(12).tolist()},
-            {'normalizedAffine': np.random.rand(12).tolist(), "timepoint": 1},
-            {'position': np.random.rand(3).tolist()},
-            {'position': np.random.rand(3).tolist(), "timepoint": 2},
-            {'normalVector': np.random.rand(3).tolist()},
+            {"affine": np.random.rand(12).tolist()},
+            {"affine": np.random.rand(12).tolist(), "timepoint": 0},
+            {"normalizedAffine": np.random.rand(12).tolist()},
+            {"normalizedAffine": np.random.rand(12).tolist(), "timepoint": 1},
+            {"position": np.random.rand(3).tolist()},
+            {"position": np.random.rand(3).tolist(), "timepoint": 2},
+            {"normalVector": np.random.rand(3).tolist()},
         ]
         for trafo in trafos:
-            view = get_default_view('image', 'my-image', viewer_transform=trafo)
-            validate_with_schema(view, 'view')
+            view = get_default_view("image", "my-image", viewer_transform=trafo)
+            validate_with_schema(view, "view")
 
         # test missing fields
         invalid_trafos = [
             {"foo": "bar"}
         ]
         for trafo in invalid_trafos:
-            view = get_default_view('image', 'my-image', viewer_transform=trafo)
+            view = get_default_view("image", "my-image", viewer_transform=trafo)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
         # test invalid fields
         invalid_trafos = [
-            {'timepoint': 0, "foo": "bar"},
-            {'affine': np.random.rand(12).tolist(), "foo": "bar"},
-            {'normalizedAffine': np.random.rand(12).tolist(), "x": "y"},
-            {'position': np.random.rand(3).tolist(), "a": 3}
+            {"timepoint": 0, "foo": "bar"},
+            {"affine": np.random.rand(12).tolist(), "foo": "bar"},
+            {"normalizedAffine": np.random.rand(12).tolist(), "x": "y"},
+            {"position": np.random.rand(3).tolist(), "a": 3}
         ]
         for trafo in invalid_trafos:
-            view = get_default_view('image', 'my-image', viewer_transform=trafo)
+            view = get_default_view("image", "my-image", viewer_transform=trafo)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
         # test invalid values
         invalid_trafos = [
-            {'timepoint': -1},
-            {'affine': np.random.rand(11).tolist()},
-            {'normalizedAffine': np.random.rand(13).tolist()},
-            {'position': np.random.rand(4).tolist()}
+            {"timepoint": -1},
+            {"affine": np.random.rand(11).tolist()},
+            {"normalizedAffine": np.random.rand(13).tolist()},
+            {"position": np.random.rand(4).tolist()}
         ]
         for trafo in invalid_trafos:
-            view = get_default_view('image', 'my-image', viewer_transform=trafo)
+            view = get_default_view("image", "my-image", viewer_transform=trafo)
             with self.assertRaises(ValidationError):
-                validate_with_schema(view, 'view')
+                validate_with_schema(view, "view")
 
     def test_get_view(self):
         from mobie.metadata import get_view, get_image_display, get_segmentation_display
@@ -365,5 +365,5 @@ class TestViewMetadata(unittest.TestCase):
         validate_with_schema(view, "view")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
