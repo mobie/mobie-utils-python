e87ad2b0f7d12ade6f1cc4e3c586fc42f0d3082c
Constantin Pape <c.pape@gmx.net>
2022-05-21 10:10:07 +0200

    Implement merge_view_file function

--- mobie/__init__.py
+++ mobie/__init__.py
@@ -1,8 +1,8 @@
 from .image_data import add_image, add_bdv_image
-from .combine_views import combine_views
 from .open_organelle import add_open_organelle_data
 from .registration import add_registered_source
 from .segmentation import add_segmentation
 from .traces import add_traces
+from .view_utils import combine_views, merge_view_file
 
 from .__version__ import __version__, SPEC_VERSION
