6c666d5fb0258507ad68ecb923cb5848dfb859fc
Constantin Pape <c.pape@gmx.net>
2022-06-28 22:58:11 +0200

    Fix issues in source renaming and add test

--- mobie/source_utils.py
+++ mobie/source_utils.py
@@ -5,11 +5,11 @@ from pybdv import metadata as bdv_metadata
 from .metadata import read_dataset_metadata, write_dataset_metadata
 
 
-def _replace_name_in_data(storage_type, rel_path, new_name):
+def _replace_name_in_data(storage_type, path, new_name):
     if "bdv" in storage_type:
-        bdv_metadata.write_name(rel_path, setup_id=0, name=new_name)
+        bdv_metadata.write_name(path, setup_id=0, name=new_name)
     elif "ome.zarr" in storage_type:
-        attrs_path = os.path.join(rel_path, ".zattrs")
+        attrs_path = os.path.join(path, ".zattrs")
         with open(attrs_path, "r") as f:
             attrs = json.load(f)
         attrs["multiscales"][0]["name"] = new_name
@@ -29,25 +29,27 @@ def _replace_name_in_view(view, old_name, new_name):
         ]
 
     displays = view.get("sourceDisplays")
-    new_displays = []
-    for display in displays:
-        # all displays have the source field
-        display_type, display = next(iter(display.items()))
-        display["sources"] = update_sources(display["sources"])
-        new_displays.append({display_type: display})
-    view["sourceDisplays"] = new_displays
+    if displays:
+        new_displays = []
+        for this_display in displays:
+            # all displays have the source field
+            display_type, this_display = next(iter(this_display.items()))
+            this_display["sources"] = update_sources(this_display["sources"])
+            new_displays.append({display_type: this_display})
+        view["sourceDisplays"] = new_displays
 
     transforms = view.get("sourceTransforms")
-    new_transforms = []
-    for transform in transforms:
-        transform_type, transform = next(iter(transform.items()))
-        # transforms have the sources or nestedSources field
-        if "sources" in transform:
-            transform["sources"] = update_sources(transform["sources"])
-        elif "nestedSources" in transform:
-            transform["nestedSources"] = update_nested_sources(transform["nestedSources"])
-        new_transforms.append({transform_type: transform})
-    view["sourceTransforms"] = new_transforms
+    if transforms:
+        new_transforms = []
+        for transform in transforms:
+            transform_type, transform = next(iter(transform.items()))
+            # transforms have the sources or nestedSources field
+            if "sources" in transform:
+                transform["sources"] = update_sources(transform["sources"])
+            elif "nestedSources" in transform:
+                transform["nestedSources"] = update_nested_sources(transform["nestedSources"])
+            new_transforms.append({transform_type: transform})
+        view["sourceTransforms"] = new_transforms
 
     return view
 
@@ -66,6 +68,7 @@ def rename_source(dataset_folder, old_name, new_name):
     if source_metadata is None:
         raise ValueError(f"The source {old_name} is not present in the dataset at {dataset_folder}")
     source_type, source_metadata = next(iter(source_metadata.items()))
+
     # sources with image data, where we need to change the name in the image data
     if source_type in ("image", "segmentation"):
         storage = source_metadata["imageData"]
@@ -73,7 +76,7 @@ def rename_source(dataset_folder, old_name, new_name):
             if "s3" in storage:
                 raise ValueError("Renaming sources that have been uploaded to s3 is not possible.")
             rel_path = rel_path["relativePath"]
-            _replace_name_in_data(storage_type, rel_path, new_name)
+            _replace_name_in_data(storage_type, os.path.join(dataset_folder, rel_path), new_name)
 
     sources[new_name] = {source_type: source_metadata}
     dataset_metadata["sources"] = sources
@@ -81,7 +84,7 @@ def rename_source(dataset_folder, old_name, new_name):
     # go through all views and rename the source there
     views = dataset_metadata["views"]
     for view_name, view in views.items():
-        view = _replace_name_in_view(view, old_name, view_name)
+        view = _replace_name_in_view(view, old_name, new_name)
 
     # rename the default view for this source (if it exists)
     view = views.pop(old_name, None)
