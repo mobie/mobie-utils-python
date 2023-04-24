3b0e82b6c9db759cc5882831da795b1de2239f4e
Martin Schorb <martin.schorb@embl.de>
2023-04-24 08:53:02 +0200

    add timepoint transformation creator

--- mobie/metadata/timepoint_view_metadata.py
+++ mobie/metadata/timepoint_view_metadata.py
@@ -1,31 +1,45 @@
-import numpy as np
-
 from .dataset_metadata import read_dataset_metadata
 from .source_metadata import get_timepoints
 from .view_metadata import get_image_display
 
 
-def get_timepoints_transform(sources, dataset_folder, start=0, end=1):
+def get_timepoints_transform(source, target, dataset_folder, sourceidx=None, targetidx=None):
+    """
+    Creates a timepoint transformation mapping timepoints from one source to timepoints of a target source
+
+    Arguments:
+        source [str] - name of the source to be mapped
+        target [str] - name of the target source
+        dataset_folder [str] - the folder for this dataset
+        sourceidx [list[int]] - indeces of the source to be mapped
+        targetidx [list[int]] - subset of target indeces
+
+    Returns:
+        list[list[int]]: the timepoint transformation
+
+    """
+
     ds = read_dataset_metadata(dataset_folder)
-    timepoints = dict()
-    len_time = dict()
 
-    for source in sources:
-        imageData = ds['sources'][source]['image']['imageData']
-        timept = get_timepoints(imageData, dataset_folder)
-        timepoints[source] = timept
-        len_time[source] = len(timept)
+    targetData = ds['sources'][target]['image']['imageData']
+    target_times = get_timepoints(targetData, dataset_folder)
 
-    if sum([val > 1 for val in len_time.values()]) > 1:
-        raise ValueError('Only one multi-timepoint source supported.')
+    imageData = ds['sources'][source]['image']['imageData']
+    timepts = get_timepoints(imageData, dataset_folder)
 
-    t_max = max(len_time.values())
+    if not sourceidx:
+        sourceidx = list(range(len(timepts)))
 
-    max_key = [key for key, value in len_time.items() if value == t_max][0]
+    if not targetidx:
+        targetidx = list(range(len(target_times)))
 
+    t_trafo = list()
 
+    for idx in targetidx:
+        s_idx = sourceidx[round(idx/len(targetidx) * len(sourceidx))]
 
-    for source in sources:
+        t_trafo.append([idx, s_idx])
+
 
 
 
